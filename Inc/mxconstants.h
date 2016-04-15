8db83ddb4bd40bf6ed3641b983cde5b97ca9eb38
Tadeusz Magura-Witkowski <teeed@na1noc.pl>
2016-04-16 01:20:28 +0200

    Initial commit

---
+++ Inc/mxconstants.h
@@ -0,0 +1,145 @@
+/**
+  ******************************************************************************
+  * File Name          : mxconstants.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define D1_4_Pin GPIO_PIN_13
+#define D1_4_GPIO_Port GPIOC
+#define D0_4_Pin GPIO_PIN_14
+#define D0_4_GPIO_Port GPIOC
+#define D1_3_Pin GPIO_PIN_15
+#define D1_3_GPIO_Port GPIOC
+#define LED_5_Pin GPIO_PIN_0
+#define LED_5_GPIO_Port GPIOF
+#define LED_4_Pin GPIO_PIN_1
+#define LED_4_GPIO_Port GPIOF
+#define LED_3_Pin GPIO_PIN_0
+#define LED_3_GPIO_Port GPIOC
+#define LED_2_Pin GPIO_PIN_1
+#define LED_2_GPIO_Port GPIOC
+#define LED_1_Pin GPIO_PIN_2
+#define LED_1_GPIO_Port GPIOC
+#define LED_0_Pin GPIO_PIN_3
+#define LED_0_GPIO_Port GPIOC
+#define TAMPER_6_Pin GPIO_PIN_1
+#define TAMPER_6_GPIO_Port GPIOA
+#define TAMPER_5_Pin GPIO_PIN_2
+#define TAMPER_5_GPIO_Port GPIOA
+#define TAMPER_4_Pin GPIO_PIN_3
+#define TAMPER_4_GPIO_Port GPIOA
+#define TAMPER_3_Pin GPIO_PIN_4
+#define TAMPER_3_GPIO_Port GPIOF
+#define TAMPER_2_Pin GPIO_PIN_5
+#define TAMPER_2_GPIO_Port GPIOF
+#define TAMPER_1_Pin GPIO_PIN_4
+#define TAMPER_1_GPIO_Port GPIOA
+#define TAMPER_0_Pin GPIO_PIN_5
+#define TAMPER_0_GPIO_Port GPIOA
+#define D0_3_Pin GPIO_PIN_6
+#define D0_3_GPIO_Port GPIOA
+#define D1_2_Pin GPIO_PIN_7
+#define D1_2_GPIO_Port GPIOA
+#define D0_2_Pin GPIO_PIN_4
+#define D0_2_GPIO_Port GPIOC
+#define D1_1_Pin GPIO_PIN_5
+#define D1_1_GPIO_Port GPIOC
+#define D0_1_Pin GPIO_PIN_0
+#define D0_1_GPIO_Port GPIOB
+#define D1_0_Pin GPIO_PIN_1
+#define D1_0_GPIO_Port GPIOB
+#define D0_0_Pin GPIO_PIN_2
+#define D0_0_GPIO_Port GPIOB
+#define OPEN_6_Pin GPIO_PIN_11
+#define OPEN_6_GPIO_Port GPIOB
+#define OPEN_0_Pin GPIO_PIN_12
+#define OPEN_0_GPIO_Port GPIOB
+#define OPEN_1_Pin GPIO_PIN_13
+#define OPEN_1_GPIO_Port GPIOB
+#define OPEN_2_Pin GPIO_PIN_14
+#define OPEN_2_GPIO_Port GPIOB
+#define OPEN_3_Pin GPIO_PIN_15
+#define OPEN_3_GPIO_Port GPIOB
+#define OPEN_4_Pin GPIO_PIN_6
+#define OPEN_4_GPIO_Port GPIOC
+#define OPEN_5_Pin GPIO_PIN_7
+#define OPEN_5_GPIO_Port GPIOC
+#define LD4_Pin GPIO_PIN_8
+#define LD4_GPIO_Port GPIOC
+#define LD3_Pin GPIO_PIN_9
+#define LD3_GPIO_Port GPIOC
+#define SWDIO_Pin GPIO_PIN_13
+#define SWDIO_GPIO_Port GPIOA
+#define BUZZER_6_Pin GPIO_PIN_6
+#define BUZZER_6_GPIO_Port GPIOF
+#define BUZZER_5_Pin GPIO_PIN_7
+#define BUZZER_5_GPIO_Port GPIOF
+#define SWCLK_Pin GPIO_PIN_14
+#define SWCLK_GPIO_Port GPIOA
+#define BUZZER_4_Pin GPIO_PIN_15
+#define BUZZER_4_GPIO_Port GPIOA
+#define D0_6_Pin GPIO_PIN_11
+#define D0_6_GPIO_Port GPIOC
+#define D0_6C12_Pin GPIO_PIN_12
+#define D0_6C12_GPIO_Port GPIOC
+#define BUZZER_3_Pin GPIO_PIN_2
+#define BUZZER_3_GPIO_Port GPIOD
+#define BUZZER_2_Pin GPIO_PIN_3
+#define BUZZER_2_GPIO_Port GPIOB
+#define BUZZER_1_Pin GPIO_PIN_4
+#define BUZZER_1_GPIO_Port GPIOB
+#define BUZZER_0_Pin GPIO_PIN_5
+#define BUZZER_0_GPIO_Port GPIOB
+#define LED_6_Pin GPIO_PIN_7
+#define LED_6_GPIO_Port GPIOB
+#define D1_5_Pin GPIO_PIN_8
+#define D1_5_GPIO_Port GPIOB
+#define D0_5_Pin GPIO_PIN_9
+#define D0_5_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
