d709064bcab1be8a89dd37331df9525034bba77e
Tadeusz Magura-Witkowski <teeed@na1noc.pl>
2016-04-16 01:56:39 +0200

    Interrupt priorities

--- Src/main.c
+++ Src/main.c
@@ -57,6 +57,7 @@ static void MX_USART1_UART_Init(void);
 
 /* USER CODE BEGIN 0 */
 
+
 /* USER CODE END 0 */
 
 int main(void)
@@ -265,13 +266,13 @@ void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(BUZZER_3_GPIO_Port, BUZZER_3_Pin, GPIO_PIN_RESET);
 
   /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI0_1_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(EXTI0_1_IRQn, 1, 0);
   HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
 
-  HAL_NVIC_SetPriority(EXTI2_3_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(EXTI2_3_IRQn, 1, 0);
   HAL_NVIC_EnableIRQ(EXTI2_3_IRQn);
 
-  HAL_NVIC_SetPriority(EXTI4_15_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(EXTI4_15_IRQn, 1, 0);
   HAL_NVIC_EnableIRQ(EXTI4_15_IRQn);
 
 }
